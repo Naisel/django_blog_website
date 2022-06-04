1a0405456335e6cf1b02e1b1a9b990aa2ffd8551
Naisel <naisel329@gmail.com>
2022-06-04 12:27:04 +0530

    added some css

--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render
 
 posts = [
     {
-        'auther' : 'naisel'
-        
+        'auther' : 'naisel',
+        'content' : 'content of blog one',
+        'date_issued' : '05-06-2000',
+        'title' : 'blog one'
+    },
+    {
+        'auther' : 'tom',
+        'content' : 'content of blog two',
+        'date_issued' : '05-06-2001',
+        'title' : 'blog two'
     }
 ]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'about'})
 
